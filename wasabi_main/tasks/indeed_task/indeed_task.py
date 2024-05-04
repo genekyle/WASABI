ffffe5caa7cfee57204c1baacb2b1f5509dcdd7e
Geno <genomags@gmail.com>
2024-05-04 08:20:48 -0400

    locations commit

--- wasabi_main/tasks/indeed_task/indeed_task.py
+++ wasabi_main/tasks/indeed_task/indeed_task.py
@@ -10,6 +10,12 @@ class IndeedTask:
         self.browser_manager = BrowserManager()
 
     @staticmethod
+    def load_locations():
+        with open(r"D:\code\WASABI\wasabi_main\tasks\indeed_task\locations.json", "r") as f:
+            data = json.load(f)
+            return data["locations"]    
+
+    @staticmethod
     def load_user_profiles():
         with open(r"D:\code\WASABI\wasabi_main\tasks\indeed_task\user_profiles.json", "r") as f:
             data = json.load(f)
@@ -17,6 +23,7 @@ class IndeedTask:
 
     async def run(self):
         selected_profile = self.task_config["selected_profile"]
+        selected_location = self.task_config["selected_location"]
 
         user_profiles = self.load_user_profiles()
         selected_user = next(profile for profile in user_profiles if profile["profile_name"] == selected_profile)
@@ -26,6 +33,8 @@ class IndeedTask:
         page = await context.new_page()
         await login.login(page)
 
+        print(f"Selected location: {selected_location}")
+
         # Implement other Indeed-specific tasks
 
         await self.browser_manager.close_browser()
@@ -34,6 +43,7 @@ class IndeedTask:
     def configuration_spec():
         user_profiles = IndeedTask.load_user_profiles()
         profile_names = [profile["profile_name"] for profile in user_profiles]
+        locations = IndeedTask.load_locations()
 
         return {
             "inputs": [
@@ -42,6 +52,12 @@ class IndeedTask:
                     "label": "Select Profile",
                     "type": "dropdown",
                     "options": profile_names
+                },
+                {
+                    "key": "selected_location",
+                    "label": "Select Location",
+                    "type": "dropdown",
+                    "options": locations
                 }
             ]
         }
